ebcbf501c460930175e9db5bbf8efc6d5a102a87
HavishVivek <havish0624@gmail.com>
2022-11-05 17:41:25 -0400

    servo

--- servo.py
+++ servo.py
@@ -1,16 +1,6 @@
-from gpiozero import AngularServo
-from time import sleep
-servo = AngularServo(17, min_angle=-90, max_angle=90)
-servo.angle = 0
+import RPi.GPIO as GPIO
+import time
 
-while True:
-    servo.angle = -90
-    sleep(2)
-    servo.angle = -45
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = 45
-    sleep(2)
-    servo.angle = 90
-    sleep(2)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setmode    
