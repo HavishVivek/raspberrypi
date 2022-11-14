4e3d7f004c253e4fcb8caeb5ccd48f728ef875ed
HavishVivek <havish0624@gmail.com>
2022-11-14 09:17:00 -0500

    rgb led

--- RGBLED.py
+++ RGBLED.py
@@ -7,3 +7,14 @@ led.red = 1
 sleep(1)
 led.red - 0.5
 sleep(1)
+
+led.color = (0, 1, 0) # full color
+sleep(1)
+led.color = (1,0,1) # magenta
+sleep(1)
+led.color = (1,1,0) # yellow
+sleep(1)
+led.color = (0,1,1) # cyan
+sleep(1)
+led.color = (1,1,1) # whilte
+sleep
